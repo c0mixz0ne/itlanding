5ce16eaa5eb07ac7febfdafd0473874bbb743d46
Антон <anton@MacBook-Air-user.local>
2023-01-17 00:51:57 +0700

    add files

---
+++ js/main.js
@@ -0,0 +1,58 @@
+window.onload = () => {
+    // console.log('allready')
+    // Timeout for example
+    setTimeout(() => document.querySelector('.preloader').remove(), 1000)
+
+    //Header action
+    window.onscroll = () => {
+        //Close menu after scroll
+        document.querySelector('.header').classList.remove('open')
+        //Go top, 300px to start
+        document.querySelector('.gotop').addEventListener('click', () => {
+            document.querySelector('#intro')
+            .scrollIntoView({behavior: "smooth"})
+        })
+        if (300 <= window.scrollY) {
+            document.querySelector('.gotop').classList.add('show')
+        }else{
+            document.querySelector('.gotop').classList.remove('show')
+        }
+        window.scrollY >= 1 ?
+        document.querySelector("header").classList.add("scroll") :
+        document.querySelector("header").classList.remove("scroll")
+
+        document.querySelectorAll('section').forEach((element, i) => {
+            if (element.offsetTop <= window.scrollY + 200 ){ // 200 = delta
+                document.querySelectorAll('nav a').forEach(element => {
+                    if (element.classList.contains('header__navigation-link_active')) {
+                        element.classList.remove('header__navigation-link_active')
+                    }
+                })
+                document.querySelectorAll('nav li')[i].querySelector('a')
+                .classList.add('header__navigation-link_active')
+            }
+        })
+    }
+
+    //Scroll to section
+    document.querySelectorAll('header a').forEach(link => {
+        link.addEventListener('click', (e) => {
+            e.preventDefault()
+            document.querySelector(link.getAttribute('href'))
+            .scrollIntoView({behavior: "smooth"})
+        })
+    })
+
+    //Subscribe
+    document.querySelector('#subscribe').addEventListener('submit', e => {
+        e.preventDefault()
+        const data = new FormData(e.target)
+        document.querySelector('.footer__link-subscribe-message')
+        .textContent = `Вы подписались на рассылку, ваша почта ${[...data.entries()][0][1]}`
+    })
+    //Menu
+    document.querySelector('.header__mobile-menu').addEventListener('click', () => {
+        document.querySelector('.header').classList.toggle('open')
+        document.querySelector('.header__navigation').style.top = `${document.querySelector('.header').offsetHeight - 1}px`
+    })
+}
